d47090bd285eeb9480b729ade230041c154ecffa
Guilherme Rocha <guilhermepradorocha@hotmail.com>
2025-09-30 20:58:55 -0300

    Fix: Remove large DB file and nested repo

--- scripts/01_load_raw_data.py
+++ scripts/01_load_raw_data.py
@@ -4,14 +4,24 @@ import os
 from sqlalchemy import create_engine
 
 data_path = 'C:/Users/User/Desktop/projects/ecommerce-analysis-sql/data'
-
+engine = create_engine(f'sqlite:///ecommerce.db')
 file_list = os.listdir(data_path)
 
 for file_name in file_list:
     if file_name.endswith('.csv'):
         full_pathway = os.path.join(data_path, file_name)
         df = pd.read_csv(full_pathway)
-        print(file_name)
+        table_name = file_name.replace('olist_','').replace('_dataset.csv','')
+        
+        df.to_sql(
+            table_name, 
+            engine, 
+            if_exists='replace', # Corrigido de 'if_exist'
+            index=False
+        )
+print("Finished!")
+
+
 
 
 # %%
